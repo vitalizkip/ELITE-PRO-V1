9c40dfa777657f967da0ea39cfe4647c4a1f18e0
vitalizkip <159427584+vitalizkip@users.noreply.github.com>
2025-04-11 17:36:37 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,11 +3,11 @@ const chalk = require('chalk')
 
 
 //contact details
-global.ownernomer = "2348109263390"
-global.ownername = "ElitePro"
+global.ownernomer = "254717296152"
+global.ownername = "Savage"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
-global.location = "Nigeria, Port Harcourt"
+global.location = "Nairobi, Kenya"
 
 
 global.ownernumber = '2348109263390'  //creator number
@@ -20,13 +20,13 @@ global.prefa = ['','!','.','#','&']
 
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
+global.autoRecording = true //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 
 
 global.autoread = false //auto read messages
-global.autobio = true //auto update bio
+global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
 global.welcome = true // or false to disable
