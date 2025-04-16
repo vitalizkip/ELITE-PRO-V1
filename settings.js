edfbcda111534bb74f6dc43ccbca487c1751e285
vitalizkip <159427584+vitalizkip@users.noreply.github.com>
2025-04-16 12:05:59 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const chalk = require('chalk')
 
 
 //contact details
-global.ownernomer = "254717296152"
-global.ownername = "Savage"
+global.ownernomer = "254741976953"
+global.ownername = "No~man"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
 global.location = "Nairobi, Kenya"
@@ -16,12 +16,12 @@ global.botname = 'ELITE-PRO-V1' //name of the bot
 
 
 //custom prefix
-global.prefa = ['','!','.','#','&']
+global.prefa = ['!','.']
 
 
 //false=disable and true=enable
-global.autoRecording = true //auto recording
-global.autoTyping = false //auto typing
+global.autoRecording = false //auto recording
+global.autoTyping = true //auto typing
 global.autorecordtype = false //auto typing + recording
 
 
@@ -29,7 +29,7 @@ global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
-global.welcome = true // or false to disable
+global.welcome = false // or false to disable
 
 //sticker details
 global.packname = 'Sticker By'
